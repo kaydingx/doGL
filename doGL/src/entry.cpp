48bcb3999889cdb736e29aaf4665ca9f1ac0f6f3
kaydingx <kayding@yeah.net>
2021-04-26 00:09:52 +0800

    first commit
    setup solution and projects base

---
+++ doGL/src/entry.cpp
@@ -0,0 +1,14 @@
+
+#include <iostream>
+#include <glad\glad.h>
+#include <GLFW\glfw3.h>
+
+
+int main()
+{
+	std::cout << "============== playGL started ==============" << std::endl;
+
+	std::cin.get();
+
+	return 0;
+}
