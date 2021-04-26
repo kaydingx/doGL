4392b6603c3058fe36fc5d6706d114be7f5f4539
kaydingx <kayding@yeah.net>
2021-04-26 21:30:02 +0800

    hello window
    1,hello window
    2,modify linker setting

--- doGL/src/entry.cpp
+++ doGL/src/entry.cpp
@@ -3,12 +3,71 @@
 #include <glad\glad.h>
 #include <GLFW\glfw3.h>
 
+const int W_WINDOW = 800;
+const int H_WINDOW = 600;
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void processInput(GLFWwindow* window);
 
 int main()
 {
 	std::cout << "============== playGL started ==============" << std::endl;
 
-	std::cin.get();
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
+	GLFWwindow* window = glfwCreateWindow(W_WINDOW, H_WINDOW, "DoGL", NULL, NULL);
+	if (window == NULL)
+	{
+		std::cout << "Failed to create GLFW window" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+
+	glfwMakeContextCurrent(window);
+
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+	{
+		std::cout << "Failed to initialize GLAD" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+
+	glViewport(0, 0, W_WINDOW, H_WINDOW);
+
+	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+
+	while (!glfwWindowShouldClose(window))
+	{
+		processInput(window);
+
+		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+
+
+	glfwTerminate();
 	return 0;
+}
+
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+	glViewport(0, 0, width, height);
+}
+
+void processInput(GLFWwindow* window)
+{
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+	{
+		glfwSetWindowShouldClose(window, true);
+	}
 }
